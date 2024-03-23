1fa847fc0d62e8c4879b6d6aa6303bac638606a3
javithbilal <javithbilal12@gmail.com>
2024-03-23 12:16:01 +0530

    adds exaple.txt

---
+++ build/assets/img/Awards & Recognition –_files/custom.js
@@ -0,0 +1,127 @@
+jQuery(function($) {
+	/*--Menu Dropdown--------*/ 
+	jQuery('.nav li.dropdown').hover(function() {
+		jQuery(this).addClass('open');
+	}, function() {
+		jQuery(this).removeClass('open');
+	}); 
+	/*-----Datepicker js------*/
+
+
+	
+	/* Demo Scripts for Bootstrap Carousel and Animate.css */
+	function doAnimations( elems ) {
+		//Cache the animationend event in a variable
+		var animEndEv = 'webkitAnimationEnd animationend';
+		
+		elems.each(function () {
+			var $this = $(this),
+			$animationType = $this.data('animation');
+			$this.addClass($animationType).one(animEndEv, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+	
+	//Variables on page load 
+	var $myCarousel = $('#home-slider'),
+	$firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+
+	//Initialize carousel 
+	$myCarousel.carousel();
+	
+	//Animate captions in first slide on page load 
+	doAnimations($firstAnimatingElems);
+	
+	//Pause carousel  
+	$myCarousel.carousel('pause');
+	
+	
+	//Other slides to be animated on carousel slide event 
+	$myCarousel.on('slide.bs.carousel', function (e) {
+		var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+		doAnimations($animatingElems);
+	});  		
+	
+
+	/*-- Page Scroll To Top Section ---------------*/
+	jQuery(window).scroll(function () {
+		if (jQuery(this).scrollTop() > 100) {
+			jQuery('.scroll-top').fadeIn();
+		} else {
+			jQuery('.scroll-top').fadeOut();
+		}
+	});
+	
+	jQuery('.scroll-top').click(function () {
+		jQuery("html, body").animate({
+			scrollTop: 0
+		}, 600);
+		return false;
+	});
+	// Home Room Carousel Js	
+	$('#home-room .item').each(function(){
+
+		var next = $(this).next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+		next.children(':first-child').clone().appendTo($(this));
+
+		for (var i=0;i<1;i++) {
+			next=next.next();
+			if (!next.length) {
+				next = $(this).siblings(':first');
+			}
+			
+			next.children(':first-child').clone().appendTo($(this));
+		}
+	});
+	// Tooltip js	
+	$('[data-toggle="tooltip"]').tooltip(); 
+	
+			//Menu fixed top		
+			var is_fixed = 0;
+			$(window).scroll(function(){
+				var scroll = $(window).scrollTop();
+				var menuHeight = $('nav.navbar').outerHeight();
+				var headerHeight = $('.header-bottom').outerHeight();
+				var totalHeight =  menuHeight + headerHeight;
+
+				if(scroll>totalHeight){
+					if(is_fixed==0)
+					{
+						is_fixed=1;
+						$('nav.top_fixed').hide().fadeIn(500).addClass('fix');
+					}
+				}else if(scroll < menuHeight){
+					is_fixed=0;
+					$('nav.top_fixed').removeClass('fix');
+				}
+			});
+            
+            
+ $('#mobile-navigation-btn').bind('click',function(){
+		$('#mobile-navigation').stop(true,true).slideToggle(300, 'easeOutBack'); //easeInOutSine works also nice at 200ms
+		return false;
+	});
+	
+	$('#mobile-navigation .container ul li').each(function(){
+		if($(this).find('> ul').length > 0) {
+			 $(this).addClass('has-ul');
+			 $(this).find('> a').append('<i class="fa fa-chevron-down"></i>');
+		}
+	});
+	
+	$('#mobile-navigation .container ul li:has(">ul") > a i').click(function(){
+		$(this).parent().parent().toggleClass('open');
+		$(this).parent().parent().find('> ul').stop(true,true).slideToggle(300, 'easeOutBack');
+		return false;
+	});
+
+	
+    
+		});
+
+
+       
